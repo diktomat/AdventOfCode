0fe0e49624befe33ad9ef3cb98bc6b37a3818eb1
Benedikt Müller <d12bb@posteo.de>
2023-03-26 17:55:16 +0200

    [2022d08] add flake and justfile

---
+++ 2022/d08/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Advent of Code 2022 Day 8";
+  inputs.utils.url = "github:numtide/flake-utils";
+  outputs = {
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [pkgs.vim pkgs.just];
+        };
+      }
+    );
+}
